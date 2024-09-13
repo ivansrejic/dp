caad94042b1a5c4b8fae2db7acb315c0d2c91c17
Srejic <srejic.ivan@hotmail.com>
2024-09-13 22:32:34 +0200

    firebase added

---
+++ dp/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: 'donna-potente-angular',
+    appId: '1:718690242063:web:7bf2d99d313dca1eeadd7a',
+    storageBucket: 'donna-potente-angular.appspot.com',
+    apiKey: 'AIzaSyAhr4WU3x-reJvvGvcw32SsDvUy7d_sc-Q',
+    authDomain: 'donna-potente-angular.firebaseapp.com',
+    messagingSenderId: '718690242063',
+  },
+};
